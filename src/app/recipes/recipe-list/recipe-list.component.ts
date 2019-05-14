0c7f976e3c5626d58cbd03530f86c27db53b6fab
KarMen <karolmendza@gmail.com>
2019-05-14 23:31:50 +0200

    working on RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import { Recipe } from "../recipe.model";
+import { Recipe } from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,14 +9,12 @@ import { Recipe } from "../recipe.model";
 })
 export class RecipeListComponent implements OnInit {
     @Output() recipeWasSelected = new EventEmitter<Recipe>();
-    recipes: Recipe[] = [
-        new Recipe('TestRecipe','This is sample description','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
-        new Recipe('AnotherTestRecipe','This is sample description','https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
-    ];
+    recipes: Recipe[];
 
-    constructor() { }
+    constructor(private recipeService: RecipeService) { }
 
     ngOnInit() {
+        this.recipes = this.recipeService.getRecipes();
     }
 
     onRecipeSelected(recipe: Recipe) {
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {Recipe} from './recipe.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipeService {
 
-  constructor() { }
+    recipes: Recipe[] = [
+        new Recipe('TestRecipe',
+            'This is sample description',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'),
+        new Recipe('AnotherTestRecipe',
+            'This is sample description',
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg')
+    ];
+
+    getRecipes() {
+        // slice to enforce copy of original array
+        return this.recipes.slice();
+    }
 }
