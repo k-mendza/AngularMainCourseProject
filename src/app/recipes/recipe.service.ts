0974b481d7b34d42f2f10a213bd458901236d0c1
k-mendza <karolmendza@gmail.com>
2019-05-16 13:37:18 +0200

    added Service Injections

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
 @Injectable({
   providedIn: 'root'
@@ -25,8 +26,16 @@ export class RecipeService {
             ])
     ];
 
+    constructor(private shoppingListService: ShoppingListService){
+
+    }
+
     getRecipes() {
         // slice to enforce copy of original array
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.shoppingListService.addIngredients(ingredients);
     }
 }
