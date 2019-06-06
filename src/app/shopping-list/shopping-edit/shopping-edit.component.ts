901381bf7324b60fb7761a8f9ae840f57986d56c
KarMen <karolmendza@gmail.com>
2019-06-06 23:02:57 +0200

    Clear button feature added

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -47,4 +47,9 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     ngOnDestroy(): void {
         this.subscription.unsubscribe();
     }
+
+    onClear() {
+        this.slForm.reset();
+        this.editMode = false;
+    }
 }
