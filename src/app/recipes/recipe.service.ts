3dc9c07a55ba3553b0acb92f292048f0ec647e42
KarMen <karolmendza@gmail.com>
2019-06-17 23:09:38 +0200

    added delete function

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -50,4 +50,9 @@ export class RecipeService {
     updateRecipe(index: number, newRecipe: Recipe) {
         this.recipes[index] = newRecipe;
     }
+
+    deleteRecipe(index: number) {
+        this.recipes.splice(index,1);
+        this.recipesChanged.next(this.recipes.slice());
+    }
 }
